407366eac7096c691d9dd02d3b207c515da7d523
Admin <jgarza145@islander.tamucc.edu>
2021-06-03 23:37:09 -0500

    created submenu items, and created transition inbetween primary menu and secondary menu

--- dropdownmenu/src/App.js
+++ dropdownmenu/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { CSSTransition} from 'react-transition-group';
 
 
 function App() {
@@ -12,7 +13,9 @@ function App() {
       <NavItem icon="😒"/>
 
       <NavItem icon="📥">
-        <p>Hello World</p>
+        <DropdownMenu>
+
+        </DropdownMenu>
       </NavItem>
       
       
@@ -23,6 +26,92 @@ function App() {
 
   
 
+  );
+}
+
+function DropdownMenu(){
+
+  const[activeMenu, setActiveMenu] = useState('main');
+  const[menuHeight, setMenuHeight] = useState(null);
+
+  function calcHeight(el){
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+
+  }
+
+  function DropdownItem(props){
+return(
+
+        <a href='#' className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+        <span className="menu-item">
+          {props.leftIcon}
+        </span>
+        {props.children}
+        <span className="icon-right">
+          {props.rightIcon}
+        </span>
+      </a>
+
+);
+  }
+
+
+
+
+
+  return(
+    <div className="dropdown" style={{height: menuHeight}}>
+
+     <CSSTransition in={activeMenu === 'main'} 
+                    unmountOnExit 
+                    timeout={500} 
+                    classNames="menu-primary" 
+                    onEnter={calcHeight}>
+
+
+
+       <div className="menu">
+
+    <DropdownItem leftIcon={'👤'}>My Profile</DropdownItem>
+
+    <DropdownItem leftIcon={'🎨'}
+                  goToMenu='settings'>
+
+      Settings
+
+
+
+    </DropdownItem>
+
+    </div>
+
+    </CSSTransition>
+
+
+
+
+
+     <CSSTransition in={activeMenu === 'settings'} 
+                    unmountOnExit 
+                    timeout={500} 
+                    classNames="menu-secondary"
+                    onEnter={calcHeight}>
+
+       <div className="menu">
+
+    <DropdownItem leftIcon={'👈'} goToMenu={'main'}>Back</DropdownItem>
+    <DropdownItem leftIcon={'🐤'}>Sounds</DropdownItem>
+    <DropdownItem leftIcon={'🔊'}> Notifictions </DropdownItem>
+    <DropdownItem leftIcon={'🔮'}> Prefrences </DropdownItem>
+
+    </div>
+
+    </CSSTransition>
+
+
+
+    </div>
   );
 }
 
